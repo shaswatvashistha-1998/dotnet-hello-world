3c63ce4a3b732b9686cc3ef5be543876ae4842f2
Shaswat Vashistha <shaswatvashistha1998@gmail.com>
2026-01-15 13:48:22 +0530

    Fix CS0104 error by replacing IHostingEnvironment with IWebHostEnvironment

--- hello-world-api/Startup.cs
+++ hello-world-api/Startup.cs
@@ -27,7 +27,7 @@ namespace hello_world_api
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
